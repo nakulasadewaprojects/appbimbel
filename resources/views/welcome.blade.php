4d736c11f67feb72afcc60ed972bccc8d4bc5b4c
zhabyans <zhafranku@gmail.com>
2019-04-23 13:22:48 +0700

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>App Bimbel</title>
     <link rel="stylesheet" href="style.css">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
