62ffae64582a80a0f798c7b1a13e66c72f0128c7
zhabyans <zhafranku@gmail.com>
2019-05-07 14:05:41 +0700

    NOTIF SISA WAKTU PENGERJAAN BIAR GAK LUPA PULANG

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <title>App Bimbel</title>
-  <link rel="stylesheet" href="style.css">
+  {{-- <link rel="stylesheet" href="css/style.css"> --}}
   <link href="{{ asset('css/style.css') }}" rel="stylesheet">
   <link rel="shortcut icon" href="assets/demo/demo6/media/img/logo/favicon.ico" />
 
@@ -17,11 +17,8 @@
   <div class="landing-page">
     <div class="page-content">
       <h1>App Bimbel</h1>
-      <p>
-        Aplikasi Bimbel Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia illo facilis non mollitia eos fuga quas
-        asperiores modi adipisci cupiditate odit rem reiciendis, natus aspernatur.
-
-      </p>
+        <h2>AKAN SEGERA LIRIS PADA</h2>
+        <h2 id="demo"></h2>
       @if (Auth::guard('web')->check())
       <a href="{{ url('/dashboard') }}">Dashboard</a> @elseif ( Auth::guard('siswa')->check())
       <a href="{{ url('/dashboardsiswa') }}">Dashboard</a> @else
@@ -35,7 +32,36 @@
     </div>
   </div>
 
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("May 29, 2019 00:00:00").getTime();
 
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + " Hari " + hours + " Jam "
+  + minutes + " Menit " + seconds + " Detik ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "PULANG";
+  }
+}, 1000);
+</script>
 </body>
 
 </html>
