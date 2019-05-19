668675b8f94d816ad295c858c1bba5cfb644fab6
rijalzulfi <rijalzulfi.mrz@gmail.com>
2019-05-19 23:06:30 +0700

    Terlewat sedikit

--- resources/views/layouts/mentor.blade.php
+++ resources/views/layouts/mentor.blade.php
@@ -541,11 +541,8 @@
 							<div class="m-menu__submenu">
 								<span class="m-menu__arrow"></span>
 								<ul class="m-menu__subnav">
-									<li class="m-menu__item " aria-haspopup="true" data-redirect="true">
+									<li class="m-menu__item  m-menu__item--parent" aria-haspopup="true"  data-redirect="true">
 										<div class="m-menu__link ">
-											<i class="m-menu__link-bullet m-menu__link-bullet--dot">
-												<span></span>
-											</i>
 											<span class="m-menu__link-text">
 												Report
 											</span>
