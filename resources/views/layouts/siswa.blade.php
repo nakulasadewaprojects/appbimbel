0fc7eb8fbcb1b72b4f9f74d1a0ad83412bb49115
zhabyans <zhafranku@gmail.com>
2019-05-22 16:01:32 +0700

    fix register siswa, ter ter ter

--- resources/views/layouts/siswa.blade.php
+++ resources/views/layouts/siswa.blade.php
@@ -370,7 +370,11 @@
 												<div class="m-dropdown__header m--align-center">
 													<div class="m-card-user m-card-user--skin-light">
 														<div class="m-card-user__pic">
-															<img src="{{ url('/data_fileSiswa/'.$isCompleted->fotoProfile) }}" height="70px" width="100px" alt="" />
+															@if(DB::table('tbdetailsiswa')->where('idtbDetailSiswa', Auth::user()->idtbSiswa)->value('fotoProfile')==NULL)
+															<img src="{{ url('/data_fileSiswa/default_photo_profile.png') }}" height="70px" width="100px" alt="Anda Belum Upload Foto"/>
+															@else
+															<a href="{{ url('/data_fileSiswa/'.$isCompleted->fotoProfile) }}" class="thumbnail"><img  height="70px" width="100px" src="{{ url('/data_fileSiswa2/'.$isCompleted->fotoProfile) }}" alt=""/></a>
+															@endif
 														</div>
 														<div class="m-card-user__details">
 															<span class="m-card-user__name m--font-weight-500">
