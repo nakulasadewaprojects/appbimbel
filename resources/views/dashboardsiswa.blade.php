237114d452877e9aed224645865a0b3a52d55724
rijalzulfi <rijalzulfi.mrz@gmail.com>
2019-05-29 11:14:27 +0700

    slide show tipis

--- resources/views/dashboardsiswa.blade.php
+++ resources/views/dashboardsiswa.blade.php
@@ -34,13 +34,13 @@
 	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 		<div class="carousel-inner">
 			<div class="carousel-item active">
-				<img class="d-block w-100" src="http://localhost/appbimbel/public/assets/img/siswa.jpg" alt="First slide">
+				<img class="d-block w-100" src="http://localhost/appbimbel/public/assets/img/1.jpg" alt="First slide">
 			</div>
 			<div class="carousel-item">
-				<img class="d-block w-100" src="http://localhost/appbimbel/public/assets/img/siswa.jpg" alt="Second slide">
+				<img class="d-block w-100" src="http://localhost/appbimbel/public/assets/img/2.jpg" alt="Second slide">
 			</div>
 			<div class="carousel-item">
-				<img class="d-block w-100" src="http://localhost/appbimbel/public/assets/img/siswa.jpg" alt="Third slide">
+				<img class="d-block w-100" src="http://localhost/appbimbel/public/assets/img/3.jpg" alt="Third slide">
 			</div>
 		</div>
 		<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
